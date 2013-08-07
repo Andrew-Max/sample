cc0504d8b3f689f96d4676ba6506aaa9edafff53
anm89 <anm898989@gmail.com>
2013-08-07 17:28:48 -0400

    Updated Dynamic Titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,13 +3,20 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Home Page" do
+
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
-    it "should have the right title" do
+
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("Mah Ruby App")
+    end
+
+    it "should have a custom title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| home')
     end
   end
 
@@ -21,19 +28,19 @@ describe "Static pages" do
     end
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("Mah Ruby App | Help")
     end
   end
 
   describe "About page" do
+
     it "should have content 'About'" do
       visit '/static_pages/about'
       expect(page).to have_content('About')
     end
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+      expect(page).to have_title("Mah Ruby App | About")
     end
   end
-
 end
