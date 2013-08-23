848c121005fd1812002b3ed7e3c2afe7093bb8ca
amax <amax@dev.(none)>
2013-08-23 18:57:41 -0400

    UP TO 7.3

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Sample::Application.routes.draw do
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
-  match '/contact', to: 'static_pages#contact', via: 'get' 
+  match '/contact', to: 'static_pages#contact', via: 'get'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name     "Michael Hartl"
-    email    "michael@example.com"
+    name     "A. Guy"
+    email    "some0@guy.com"
     password "foobar"
     password_confirmation "foobar"
   end
