437e4fde72d1f8ca213e31e8ae7fc01479fe1970
anm89 <anm898989@gmail.com>
2013-08-26 02:41:21 -0400

    most of the way through 9: view users, current user authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
-  
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def new
     @user = User.new
+  end
+  
+  def index
+    @users = User.all
   end
 
   def show
@@ -20,11 +26,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       sign_in @user
@@ -41,4 +45,18 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
   end
+
+  # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to sign_in_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
