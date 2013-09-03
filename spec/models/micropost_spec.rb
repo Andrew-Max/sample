303d8bcf6d3fecb9a77dcfded48b44c11a809095
anm89 <amax@forever.com>
2013-09-03 16:09:31 -0400

    minor fixes

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -30,7 +30,7 @@ describe Micropost do
   end
 
   describe "with content that is too long" do
-    before { @micropost.content = "a" * 141 }
+    before { @micropost.content = "a" * 541}
     it { should_not be_valid }
   end
 end
